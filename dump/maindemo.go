21b167e3b84cf71af208999ad7feea46141f72b6
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2021-04-08 16:49:32 -0700

    code cleanup

--- dump/maindemo.go
+++ dump/maindemo.go
@@ -1,1 +1,36 @@
-package dump
+package main
+
+import (
+	"github.com/hcasalet/lazycert/dump/lc"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+/*func main() {
+	//// Open the Badger database located in the /tmp/badger directory.
+	//// It will be created if it doesn't exist.
+	//db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer db.Close()
+	//// Your code here…
+	d := lc.Dummy{}
+	fmt.Println(d)
+
+
+}*/
+
+func main() {
+	lis, err:= net.Listen("tpc", "35000")
+	if err!=nil {
+		log.Fatalf("Error starting the server at port 350000, %v", err)
+	}
+
+	s := grpc.NewServer()
+	lc.RegisterEdgeNodeServer(s, &lc.Server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/hcasalet/lazycert
 
 go 1.15
+
+require (
+	github.com/armon/go-metrics v0.3.6
+	github.com/dgraph-io/badger/v3 v3.2011.1
+	github.com/golang/protobuf v1.4.2
+	github.com/hashicorp/go-hclog v0.16.0
+	github.com/hashicorp/go-msgpack v1.1.5
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974
+	google.golang.org/grpc v1.37.0
+)
