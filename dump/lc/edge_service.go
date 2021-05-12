28e8e3bd77f7abe13f184c08be8ff471e4018536
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2021-05-12 01:35:36 -0700

    modified proto file to update inputs args to READ and COMMIT API for edge nodes. Implemented rudimentary read operation.

--- dump/lc/edge_service.go
+++ dump/lc/edge_service.go
@@ -19,26 +19,35 @@ func NewEdgeService(config *Config) *EdgeService {
 	}
 }
 
-func (s EdgeService) Commit(ctx context.Context, val *KeyVal) (*Dummy, error) {
+func (e *EdgeService) Commit(ctx context.Context, val *CommitData) (*Dummy, error) {
 	panic("implement me")
 }
 
-func (s EdgeService) Read(ctx context.Context, val *KeyVal) (*KeyVal, error) {
+func (e *EdgeService) Read(ctx context.Context, val *KeyVal) (*ReadResponse, error) {
+	key := string(val.Key)
+	if v, ok := e.dbDict[key]; ok {
+		return &ReadResponse{
+			Data: &KeyVal{
+				Key:   val.Key,
+				Value: v,
+			}, Status: ResponseStatus_SUCCESS}, nil
+	} else {
+		return &ReadResponse{Data: &KeyVal{Key: val.Key, Value: []byte{0}}, Status: ResponseStatus_ERROR}, nil
+	}
+}
+
+func (e *EdgeService) Propose(ctx context.Context, data *ProposeData) (*Dummy, error) {
 	panic("implement me")
 }
 
-func (s EdgeService) Propose(ctx context.Context, data *ProposeData) (*Dummy, error) {
+func (e *EdgeService) HeartBeat(ctx context.Context, info *HeartBeatInfo) (*Dummy, error) {
 	panic("implement me")
 }
 
-func (s EdgeService) HeartBeat(ctx context.Context, info *HeartBeatInfo) (*Dummy, error) {
+func (e *EdgeService) Certification(ctx context.Context, certificate *Certificate) (*Dummy, error) {
 	panic("implement me")
 }
 
-func (s EdgeService) Certification(ctx context.Context, certificate *Certificate) (*Dummy, error) {
-	panic("implement me")
-}
-
-func (s EdgeService) LeaderStatus(ctx context.Context, config *LeaderConfig) (*Dummy, error) {
+func (e *EdgeService) LeaderStatus(ctx context.Context, config *LeaderConfig) (*Dummy, error) {
 	panic("implement me")
 }
